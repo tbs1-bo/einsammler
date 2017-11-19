aacd5e345fc7b423fc5f0004ecb10d8c1cbaa6c8
Marco Bakera <marco@bakera.de>
2017-11-19 12:48:43 +0100

    added whitespaces.

--- einsammler.sh
+++ einsammler.sh
@@ -25,11 +25,14 @@ while true; do
     MOUNTED_TGT=$(findmnt LABEL=$LBL --noheadings --output TARGET)
     if [[ -n $MOUNTED_TGT ]]; then
 	echo "USB-Stick with label $LBL found at $MOUNTED_TGT."
+
 	TGTDIR=$TGT/$(date +'%Y-%m-%dT%H-%M-%S')
 	echo "Creating $TGTDIR"
-	mkdir -p $TGTDIR	
+	mkdir -p $TGTDIR
+
 	echo "Moving files"
 	mv -n -v $MOUNTED_TGT/* $TGTDIR
+
 	echo "Unmounting device"
 	umount $MOUNTED_TGT && echo "succeeded"
 	echo '-----'
